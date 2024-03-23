c000b4caf56fc4b932111d15cd01ddbb34bc8ed3
zaputlah <reza.rr889@gmail.com>
2024-03-23 12:04:01 +0700

    first

--- main.js
+++ main.js
@@ -2,67 +2,67 @@ $(document).ready(function ($) {
     "use strict";
 
 
-    var book_table = new Swiper(".book-table-img-slider", {
-        slidesPerView: 1,
-        spaceBetween: 20,
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        speed: 2000,
-        effect: "coverflow",
-        coverflowEffect: {
-            rotate: 3,
-            stretch: 2,
-            depth: 100,
-            modifier: 5,
-            slideShadows: false,
-        },
-        loopAdditionSlides: true,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-    });
+    // var book_table = new Swiper(".book-table-img-slider", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 20,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 3000,
+    //         disableOnInteraction: false,
+    //     },
+    //     speed: 2000,
+    //     effect: "coverflow",
+    //     coverflowEffect: {
+    //         rotate: 3,
+    //         stretch: 2,
+    //         depth: 100,
+    //         modifier: 5,
+    //         slideShadows: false,
+    //     },
+    //     loopAdditionSlides: true,
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         clickable: true,
+    //     },
+    // });
 
-    var team_slider = new Swiper(".team-slider", {
-        slidesPerView: 3,
-        spaceBetween: 30,
-        loop: true,
-        autoplay: {
-            delay: 3000,
-            disableOnInteraction: false,
-        },
-        speed: 2000,
+    // var team_slider = new Swiper(".team-slider", {
+    //     slidesPerView: 3,
+    //     spaceBetween: 30,
+    //     loop: true,
+    //     autoplay: {
+    //         delay: 3000,
+    //         disableOnInteraction: false,
+    //     },
+    //     speed: 2000,
 
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        breakpoints: {
-            0: {
-                slidesPerView: 1.2,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            992: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 3,
-            },
-        },
-    });
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         clickable: true,
+    //     },
+    //     breakpoints: {
+    //         0: {
+    //             slidesPerView: 1.2,
+    //         },
+    //         768: {
+    //             slidesPerView: 2,
+    //         },
+    //         992: {
+    //             slidesPerView: 3,
+    //         },
+    //         1200: {
+    //             slidesPerView: 3,
+    //         },
+    //     },
+    // });
 
     jQuery(".filters").on("click", function () {
         jQuery("#menu-dish").removeClass("bydefault_show");
@@ -80,7 +80,7 @@ $(document).ready(function ($) {
                         easing: "ease-in-out",
                     },
                     load: {
-                        filter: ".all, .breakfast, .lunch, .dinner",
+                        filter: ".all, .soto, .ayam, .lele, .minumandingin, .minumanhangat",
                     },
                 });
             },
@@ -169,4 +169,90 @@ jQuery(window).on('load', function () {
         }
 
     }
+});
+
+const typedTextSpan = document.getElementById('typed-text');
+const textArray = ["Soto Ayam Salimnur"];
+const typingDelay = 200; // in milliseconds
+const erasingDelay = 100; // in milliseconds
+const newTextDelay = 2000; // in milliseconds
+
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex++;
+        if (textArrayIndex >= textArray.length) textArrayIndex = 0;
+        setTimeout(type, typingDelay);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+    if (textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+function changeMainImage(index) {
+    var mainImage = document.getElementById('mainImage');
+    var newImageUrl;
+
+    // Tentukan URL gambar baru berdasarkan indeks yang diterima
+    if (index === 0) {
+        newImageUrl = 'assets/images/ayam-goreng.jpg';
+    } else if (index === 1) {
+        newImageUrl = 'assets/images/ayambakarsalimnur.jpg';
+    } else if (index === 2) {
+        newImageUrl = 'assets/images/lelebakarsalimnur.jpg';
+    } else if (index === 3) {
+        newImageUrl = 'assets/images/lelegorengsalimnur.jpg';
+    } else if (index === 4) {
+        newImageUrl = 'assets/images/Soto-Ayam-telur.jpeg';
+    } else if (index === 5) {
+        newImageUrl = 'assets/images/sotoayamcekerssalimnur.jpg';
+    }
+
+    // Ubah gambar utama dengan gambar yang baru
+    mainImage.style.backgroundImage = "url('" + newImageUrl + "')";
+}
+
+ function initMap() {
+        // Inisialisasi peta
+    var map = L.map('map').setView([-6.175392093812001, 107.00696287498992], 15); // Atur koordinat dan level zoom sesuai kebutuhan Anda
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    var marker = L.marker([-6.175392093812001, 107.00696287498992]).addTo(map);
+    marker.bindPopup("<b>Our Restaurant</b>").openPopup();
+
+    }
+
+    document.querySelectorAll('nav.header-menu a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+        }
+    });
 });
